53f26468800e0e1a6c6a8d33bec9bee053bb8d15
Harold Asbridge <hasbridge@gmail.com>
2014-06-10 16:52:49 -0500

    Create rc-nav-led.ino

---
+++ rc-nav-led.ino
@@ -0,0 +1,64 @@
+/*
+****************************************************
+RC LED Navigation Lights
+
+Fades in and out an LED to simulate a rotating anti-collision beacon
+Blinks strobe LED(s)
+ 
+@author Harold Asbridge
+@version 0.1
+@date 2014-06-07
+****************************************************
+*/
+ 
+// Anti-collision LED pin number - needs to support PWM
+int acLedPin = 3;
+
+// Strobe LED pin number
+int strobeLedPin = 4;
+
+int minFade = 5;
+int maxFade = 127;
+int fadeInterval = 50;
+
+// Don't change, these are used to store the current state of the anti-collision LED
+int currentFade = minFade + 1;
+int dir = 1;
+
+void setup() {
+  // initialize our pins as output
+  pinMode(acLedPin, OUTPUT);
+  pinMode(strobeLedPin, OUTPUT);
+}
+  
+void loop() {
+  // Fade in anti-collision LED
+  int i;
+  for (i = minFade; i <= maxFade; i+= 5) {
+    analogWrite(acLedPin, i);
+    delay(fadeInterval);
+  }
+  
+  // Blink anti-collision and strobe once
+  digitalWrite(strobeLedPin, HIGH);
+  analogWrite(acLedPin, 255);
+  delay(100);
+  
+  // Turn off strobe
+  digitalWrite(strobeLedPin, LOW);
+  
+  // Put anti-collision back at the max fade brightness
+  analogWrite(acLedPin, maxFade);
+  
+  // Flash strobe again
+  delay(50);
+  digitalWrite(strobeLedPin, HIGH);
+  delay(100);
+  digitalWrite(strobeLedPin, LOW);
+  
+  // Fade out anti-collision LED
+  for (i = maxFade; i >= minFade; i-= 5) {
+    analogWrite(acLedPin, i);
+    delay(fadeInterval); 
+  }
+}
